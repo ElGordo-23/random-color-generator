ce95af88988ca165a19089c3550cdb8d90d6e728
Felix Lang <felix.lang@mailbox.org>
2021-09-07 17:22:35 +0200

    Mad the script work with chalk

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-// const randomColor = require('randomcolor');
+const randomColor = require('randomcolor');
 const chalk = require('chalk');
-const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+const freestyleColor = Math.floor(Math.random() * 16777215).toString(16);
 
-console.log(chalk.hex(randomColor)('#############################'));
-console.log(chalk.hex(randomColor)('#############################'));
-console.log(chalk.hex(randomColor)('#############################'));
-console.log(
-  chalk.hex(randomColor)('########   ' + '#' + randomColor + '   ########'),
-);
-console.log(chalk.hex(randomColor)('#############################'));
-console.log(chalk.hex(randomColor)('#############################'));
-console.log(chalk.hex(randomColor)('#############################'));
+let hue = process.argv[2];
+let luminosity = process.argv[3];
+
+const userColor = randomColor({
+  luminosity: luminosity,
+  hue: hue,
+});
+
+if (process.argv[2]) {
+  console.log(chalk.hex(userColor)('#############################'));
+  console.log(chalk.hex(userColor)('#############################'));
+  console.log(chalk.hex(userColor)('#############################'));
+  console.log(
+    chalk.hex(userColor)('########   ' + '#' + freestyleColor + '   ########'),
+  );
+  console.log(chalk.hex(userColor)('#############################'));
+  console.log(chalk.hex(userColor)('#############################'));
+  console.log(chalk.hex(userColor)('#############################'));
+} else {
+  console.log(chalk.hex(freestyleColor)('#############################'));
+  console.log(chalk.hex(freestyleColor)('#############################'));
+  console.log(chalk.hex(freestyleColor)('#############################'));
+  console.log(
+    chalk.hex(freestyleColor)(
+      '########   ' + '#' + freestyleColor + '   ########',
+    ),
+  );
+  console.log(chalk.hex(freestyleColor)('#############################'));
+  console.log(chalk.hex(freestyleColor)('#############################'));
+  console.log(chalk.hex(freestyleColor)('#############################'));
+}
