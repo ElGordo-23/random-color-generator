6e2d0ae60ed9e139d5818f7bc29b4aa11cd36ea6
Felix Lang <felix.lang@mailbox.org>
2021-09-08 13:36:46 +0200

    Update to final pretty version using template strings.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const randomColor = require('randomcolor');
 const chalk = require('chalk');
-const freestyleColor = Math.floor(Math.random() * 16777215).toString(16);
+// Generates a random hex number, might be useful in the future:
+// const freestyleColor = Math.floor(Math.random() * 16777215).toString(16);
 
 const hue = process.argv[2];
 const luminosity = process.argv[3];
@@ -11,25 +12,33 @@ const userColor = randomColor({
 });
 
 if (process.argv[2]) {
-  console.log(chalk.hex(userColor)('#############################'));
-  console.log(chalk.hex(userColor)('#############################'));
-  console.log(chalk.hex(userColor)('#############################'));
   console.log(
-    chalk.hex(userColor)('########   ' + '#' + userColor + '  ########'),
+    chalk.hex(userColor)(`
+  #############################
+  #############################
+  #############################
+  #############################
+  #############################
+  ######     ${userColor}    #######
+  #############################
+  #############################
+  #############################
+  #############################
+  #############################`),
   );
-  console.log(chalk.hex(userColor)('#############################'));
-  console.log(chalk.hex(userColor)('#############################'));
-  console.log(chalk.hex(userColor)('#############################'));
 } else {
-  console.log(chalk.hex(freestyleColor)('#############################'));
-  console.log(chalk.hex(freestyleColor)('#############################'));
-  console.log(chalk.hex(freestyleColor)('#############################'));
   console.log(
-    chalk.hex(freestyleColor)(
-      '########   ' + '#' + freestyleColor + '   ########',
-    ),
+    chalk.hex(userColor)(`
+ #############################
+ #############################
+ #############################
+ #############################
+ #############################
+ ######     ${userColor}    #######
+ #############################
+ #############################
+ #############################
+ #############################
+ #############################`),
   );
-  console.log(chalk.hex(freestyleColor)('#############################'));
-  console.log(chalk.hex(freestyleColor)('#############################'));
-  console.log(chalk.hex(freestyleColor)('#############################'));
 }
